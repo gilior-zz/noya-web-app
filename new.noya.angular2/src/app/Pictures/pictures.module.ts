d5e82706bb867896a54e52056848f4878309ed74
gilior <liorgish@gmail.com>
2017-04-12 07:13:56 +0300

    dummy

--- new.noya.angular2/src/app/Pictures/pictures.module.ts
+++ new.noya.angular2/src/app/Pictures/pictures.module.ts
@@ -1,6 +1,6 @@
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 import { PicturesRoutingModule } from './pictures-routing.module';
 import { Pictures } from "./pictures";
--- new.noya.angular2/src/app/routing/app.routes.ts
+++ new.noya.angular2/src/app/routing/app.routes.ts
@@ -1,43 +1,35 @@
-﻿import { PicturesModule } from './../Pictures/pictures.module';
-import { VideosModule } from './../Videos/videos.module';
-import { ProgramsModule } from './../Programs/programs.module';
-
-import { LinksModule } from './../Links/links.module';
-import { ContactModule } from './../Contact/contact.module';
-
-
-
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { RouterModule, CanDeactivate } from '@angular/router';
 
 
 
-import { Pictures } from "../Pictures/pictures"
 
-import { HeaderImage } from "../HeaderImage/header.image"
-import { CanDeactivateGuard } from "../common/can-deactivate-guard.service";
+
+import { HeaderImage } from '../HeaderImage/header.image';
+import { CanDeactivateGuard } from '../common/can-deactivate-guard.service';
+
+
 
 
 @NgModule({
-    imports: [
-        
-        RouterModule.forRoot([
-          
-            { path: "home",loadChildren:'./../Home/home.module#HomeModule' },
-            { path: "biography",loadChildren:'./../Biography/biography.module#BiographyModule' },
-            { path: "contact",loadChildren:'./../Contact/contact.module#ContactModule' },         
-              { path: 'links',loadChildren:'./../Links/links.module#LinksModule'},
-                { path: 'programs',loadChildren:'./../Programs/programs.module#ProgramsModule'},
-                  { path: 'videos',loadChildren:'./../Videos/videos.module#VideosModule'},
-            { path: "pictures", loadChildren: './../Pictures/pictures.module#PicturesModule' },                    
-                    
+    imports:  [
+        RouterModule.forRoot( 
+            [
+            { path: 'home', loadChildren: './../Home/home.module#HomeModule' },
+            { path: 'biography', loadChildren: './../Biography/biography.module#BiographyModule' },
+            { path: 'contact', loadChildren: './../Contact/contact.module#ContactModule'},
+              { path: 'links', loadChildren: './../Links/links.module#LinksModule'},
+                { path: 'programs', loadChildren: './../Programs/programs.module#ProgramsModule'},
+                  { path: 'videos', loadChildren: './../Videos/videos.module#VideosModule'},
+            { path: 'pictures', loadChildren: './../Pictures/pictures.module#PicturesModule'},
             { path: '', redirectTo: 'home', pathMatch: 'full' }
-        ], {useHash:true})
+        ], {useHash: true}
+        )
     ],
     exports: [
         RouterModule
     ],
-    providers: [ 
+    providers:[
         CanDeactivateGuard
     ]
 })
