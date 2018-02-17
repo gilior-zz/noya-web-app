a34f591462ef85b5e31bcd79e89dbe4de690ce51
lior gishry <liorgish@gmail.com>
2018-02-17 23:29:58 +0200

    node

--- new.noya.angular2/server/config.ts
+++ new.noya.angular2/server/config.ts
@@ -1,4 +1,4 @@
-export const DBConfig={
+export const DBConfig = {
   userName: 'lior',
   password: 'lM8%px35',
   server: '188.121.44.212',
@@ -6,3 +6,10 @@ export const DBConfig={
   // If you're on Windows Azure, you will need this:
   options: {encrypt: true}
 }
+
+export const emailConfig = {
+  subject: 'message from my web site',
+  apiKey: 'key-7fa3cfafbfdc3a2a28fec32029c88742',
+  domain: 'sandbox185e99a6024a47928602c0edc1188d3e.mailgun.org',
+  to: 'liorgish@gmail.com'
+};
